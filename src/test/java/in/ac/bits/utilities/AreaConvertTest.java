906ccc6178d81183f2ac39c1f537568e2d2b438c
mittPrateek <prateek_pd@yahoo.co.in>
2022-05-08 12:19:53 +0530

    Test for area converter

---
+++ src/test/java/in/ac/bits/utilities/AreaConvertTest.java
@@ -0,0 +1,29 @@
+/*
+ * Author Prateek Mittal
+ */
+
+package in.ac.bits.utilities;
+
+import in.ac.bits.utilities.AreaUtil;
+import org.junit.jupiter.api.Test;
+
+
+public class AreaConverterTest {
+
+    @Test
+    public void sqkToAcres() {
+        double Acre = new AreaUtil.Area().fromSquareMeters(1.25).toAcres();;
+        assert(Acre);
+    }
+    
+    @Test
+    public void frmSqMetertoSqKilometer() {
+        AreaUtil.Area sqm = new AreaUtil.Area().fromSquareMeters(1.25);
+        double sqkm = sqm.toSquareKilometers();
+        assert(sqkm);
+
+    }
+}
+
+
+
