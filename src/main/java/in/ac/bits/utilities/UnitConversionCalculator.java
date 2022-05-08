acbc938bb6836aa350138d5960aa566953d5d58e
Ravirai2304 <63184514+Ravirai2304@users.noreply.github.com>
2022-05-08 21:49:50 +0530

    Updated Unit Converter Java Utility

--- src/main/java/in/ac/bits/utilities/UnitConversionCalculator.java
+++ src/main/java/in/ac/bits/utilities/UnitConversionCalculator.java
@@ -9,7 +9,6 @@ public class UnitConversionCalculator {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         System.out.println("Convert from:");
         String fromUnit = in.nextLine();
         System.out.println("Convert to: ");
